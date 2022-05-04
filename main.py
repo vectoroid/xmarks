e5c006de89714dd71100ed33a0838f0618325fdd
Mark Aurelius <tmalone@protonmail.ch>
2022-05-04 10:39:21 +0000

    Added diagnostic route for inspecting Deta environment variables, config, etc.

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
+import datetime
 import fastapi
+import hashlib
+import os
+import sys
 
 app = fastapi.FastAPI()
+
+async def get_fake_passkey():
+    m = hashlib.sha3_512()
+    m.update('Deta rocks! The time is now ' + datetime.datetime().now())
+    return m.digest()
 
 @app.get('/')
 async def read_root():
     return {'data': {
         'message': "Hello, world!"
+    }}
+
+@app.get('diagnostix')
+async def read_diagnostix():
+    return {'data': {
+        'deta': {
+            'path': os.environ['DETA_PATH'],
+            'python_version': sys.version,
+            'running_on_micro': os.environ['DETA_RUNTIME'],
+            'key': get_fake_passkey()
+        }
     }}
